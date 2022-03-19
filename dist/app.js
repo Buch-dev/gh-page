e8b8eb130196038b9dfb7d54050c4e9598e1d839
Daniel Okafor <bucheed@gmail.com>
2022-03-19 14:12:31 +0100

    javascript update

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
-const body = document.getElementsId('bdy');
+
 
 document.getElementById('btn').addEventListener('click', changeBackground);
 
 function changeBackground() {
-    body.classList.add('background-color');
+    document.body.style.backgroundColor = 'red';
 }
